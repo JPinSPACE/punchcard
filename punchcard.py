4e30fae0e21b949b3d500ec6977e46c553841400
JPinSPACE <jp@jpinspace.com>
2014-04-19 22:54:58 -0400

    controller - Continue work on transforming data into something useful

--- punchcard.py
+++ punchcard.py
@@ -1,20 +1,31 @@
 import model
 import datetime
+import json
 
 GRID_LENGTH = 20
 
 def get_data():
     raw_data = {}
     values = {}
+    views = {}
+    max_value = {}
+
+    transformed = {}
 
     seqs = model.get_sequence_names()
 
     # retrieve data and build dictionary keyed on date
     for seq in seqs:
+        max_value[seq] = 0
         values[seq] = []
+        views[seq] = {}
 
         seq_dict = {}
         seq_data = model.get_sequence_data(seq)
+
+        view = model.get_view(seq)
+        views[seq]['type'] = view[0]
+        views[seq]['color_info'] = view[1]
 
         for datum in seq_data:
             seq_dict[datum[0]] = datum[1]
@@ -30,19 +41,36 @@ def get_data():
 
         for seq in seqs:
             if date in raw_data[seq].keys():
+                # since we're already iterating over this we may
+                # as well grab the maximum value now
+                if raw_data[seq][date] > max_value[seq]:
+                    max_value[seq] = raw_data[seq][date]
                 values[seq].append(raw_data[seq][date])
             else:
                 values[seq].append(0)
 
         d -= day
 
-    return values
+    for seq in seqs:
+        transformed[seq] = []
+        color_info = json.loads(views[seq]['color_info'])
+
+        if views[seq]['type'] == 'binary':
+            for value in values[seq]:
+                if value == 1:
+                    transformed[seq].append(color_info['color'])
+                else:
+                    transformed[seq].append([0, 0, 0])
+
+    print transformed
+
 
 
 
         
 
 def main():
-    values = get_data()
+    data = get_data()
+
 
 main()
